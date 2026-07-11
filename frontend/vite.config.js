21a5dc7fa1ab8842427fc9917689c15cfa4b0457
yw0ns <development@tlnzone.de>
2026-07-11 23:10:36 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/VN-flight-tracker-limited/',  // Match your GitHub repo name
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  
 })
